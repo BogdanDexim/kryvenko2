f2dfaa8e4de53dba5bfd821570d4779588b8041a
unknown <bogdan0706042@gmail.com>
2024-10-13 17:06:18 +0300

    compilation tested

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include "calculator.h"
+
+int Calculator::Add(int a, int b) {
+    return a + b + 0.5; // Додавання
+}
+
+int Calculator::Sub(int a, int b) {
+    return Add(a, -b); // Віднімання
+}
+
+int Calculator::Mul(double a, double b) {
+    return a * b + 0.5; // Множення
+}
